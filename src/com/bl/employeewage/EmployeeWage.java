480c903d6ee385dd83f4d4bb5a7dc79d49d22614
lunainfotec <kirtimadan1997@gmail.com>
2023-04-15 01:05:09 +0530

    added : Employee wage part time for month

--- src/com/bl/employeewage/EmployeeWage.java
+++ src/com/bl/employeewage/EmployeeWage.java
@@ -1,37 +1,45 @@
 package com.bl.employeewage;
 
 public class EmployeeWage {
+
     public static final int IS_FULL_TIME = 2;
-    public static final int  IS_PART_TIME = 1;
-    public static final int  EMP_RATE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int EMP_WORKING_DAY = 20;
+
     public static void main(String[] args) {
 
         int empHrs = 0;
         int empWage = 0;
+        int totalempWage = 0;
+
+        for (int day = 0; day < EMP_WORKING_DAY; day++) {
+
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+
+            switch (empCheck) {
+
+                case IS_PART_TIME:
+
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+
+                    empHrs = 8;
+                    break;
 
 
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+                default:
+                    empHrs = 0;
 
-        switch (empCheck){
-
-            case IS_FULL_TIME:
-
-                empHrs = 8;
-                break;
-
-            case IS_PART_TIME:
-
-                empHrs = 4;
-                break;
-
-            default:{
-                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalempWage = totalempWage + empWage;
+            System.out.println("Emp Wage:" + empWage);
         }
 
+        System.out.println("total wage" + totalempWage);
 
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-
-        System.out.println("Emp Wage:" + empWage);
     }
 }
